a38f734df668635e39742db6ce79df1a75ef14cc
yesidmelo <yesid.melo@pragma.com.co>
2023-05-30 15:08:43 -0500

    implementacion de manejo en ladscape y portrait. no esta manteniendo el estado del juego en estos momentos

--- app/src/main/java/com/pragma/ticktactoe/layouts/Encabezado.kt
+++ app/src/main/java/com/pragma/ticktactoe/layouts/Encabezado.kt
@@ -1,9 +1,10 @@
 package com.pragma.ticktactoe.layouts
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,21 +13,20 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import com.pragma.ticktactoe.constantes.EstadoJuegoEnum
-import com.pragma.ticktactoe.ui.theme.Black
 import com.pragma.ticktactoe.ui.theme.MyTypography
 import com.pragma.ticktactoe.ui.theme.PurpleTextColor
 import com.pragma.ticktactoe.ui.theme.WhiteButtons
 import com.pragma.ticktactoe.ui.theme.WhiteButtons1
-import com.pragma.ticktactoe.utils.advancedShadow
 
-@Preview(showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_NO, device = Devices.PHONE, showBackground = true)
+@Preview(uiMode = UI_MODE_NIGHT_NO, device = Devices.TABLET, widthDp = 891, heightDp = 411 , showBackground = true)
 @Composable
 fun prevEncabezado() {
     Encabezado(modifier = Modifier.fillMaxSize(), turnoActual = EstadoJuegoEnum.TURNO_JUGADOR2)
@@ -38,6 +38,51 @@ fun Encabezado(
     modifier: Modifier,
     turnoActual : EstadoJuegoEnum
 ) {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            EncabezadoLandscape(modifier = modifier, turnoActual = turnoActual)
+        }
+        else -> {
+            EncabezadoPortrait(modifier = modifier, turnoActual = turnoActual)
+        }
+    }
+}
+
+@Composable
+fun EncabezadoLandscape(modifier: Modifier,
+             turnoActual : EstadoJuegoEnum){
+    Box(modifier = modifier.fillMaxSize()) {
+        ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+            val (boton1, boton2) = createRefs()
+            customButtonTurn(
+                modifier = Modifier.constrainAs(boton1) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(boton2.top)
+                },
+                text = "Jugador 1 O",
+                turnoActual = turnoActual,
+                jugadorBoton = EstadoJuegoEnum.TURNO_JUGADOR1)
+
+            customButtonTurn(
+                modifier = Modifier.constrainAs(boton2){
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    top.linkTo(boton1.top)
+                },
+                text = "Jugador 2 X",
+                turnoActual = turnoActual,
+                jugadorBoton = EstadoJuegoEnum.TURNO_JUGADOR2)
+        }
+    }
+}
+
+@Composable
+fun EncabezadoPortrait(modifier: Modifier,
+             turnoActual : EstadoJuegoEnum){
     Box(modifier = modifier.fillMaxSize()) {
         ConstraintLayout(modifier = Modifier.fillMaxSize()) {
             val (boton1, boton2) = createRefs()
@@ -65,6 +110,7 @@ fun Encabezado(
         }
     }
 }
+
 
 @Composable
 fun customButtonTurn(modifier: Modifier, text:String, turnoActual: EstadoJuegoEnum, jugadorBoton: EstadoJuegoEnum){
--- app/src/main/java/com/pragma/ticktactoe/layouts/TicTacToeScreen.kt
+++ app/src/main/java/com/pragma/ticktactoe/layouts/TicTacToeScreen.kt
@@ -1,5 +1,6 @@
 package com.pragma.ticktactoe.layouts
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,7 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.lifecycle.MutableLiveData
 import com.pragma.ticktactoe.constantes.EstadoJuegoEnum
 import com.pragma.ticktactoe.constantes.JugadorCasillaEnum
@@ -18,14 +23,16 @@ import com.pragma.ticktactoe.mvvm.JuegoViewModel
 import com.pragma.ticktactoe.ui.theme.Purple80
 
 @SuppressWarnings("kotlin:S1186")
-@Preview(showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, device = Devices.PHONE, showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO, device = Devices.TABLET, widthDp = 891, heightDp = 411 , showBackground = true)
 @Composable
 fun prevScreenTictactoe() {
     val estadoTablero = MutableLiveData<MutableList<DetalleCasillaTriqui>>()
     val ganadorJuego = MutableLiveData<JugadorCasillaEnum>()
     val turnoActual = MutableLiveData<EstadoJuegoEnum>()
-    ganadorJuego.value = JugadorCasillaEnum.NINGUNO
-    TicTacToeScreen(juegoViewModel = object : JuegoViewModel() {
+    ganadorJuego.value = JugadorCasillaEnum.EMPATE
+
+    val viewModel = object : JuegoViewModel() {
         override fun estadoActualTablero()=estadoTablero
 
         override fun ganadorDelJuego() = ganadorJuego
@@ -41,11 +48,25 @@ fun prevScreenTictactoe() {
 
         override fun turnoActual() = turnoActual
 
-    })
+    }
+    TicTacToeScreen(juegoViewModel = viewModel)
 }
 
 @Composable
 fun TicTacToeScreen(juegoViewModel: JuegoViewModel) {
+    val configuration = LocalConfiguration.current
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> {
+            TictactoeLandscape(juegoViewModel = juegoViewModel)
+        }
+        else -> {
+            TictactoePortrait(juegoViewModel = juegoViewModel)
+        }
+    }
+}
+
+@Composable
+fun TictactoePortrait(juegoViewModel: JuegoViewModel) {
     val estadoActualTablero : MutableList<DetalleCasillaTriqui> by juegoViewModel.estadoActualTablero().observeAsState(initial = emptyList<DetalleCasillaTriqui>().toMutableList())
     val ganadorJuego: JugadorCasillaEnum by juegoViewModel.ganadorDelJuego().observeAsState(initial = JugadorCasillaEnum.NINGUNO)
     val turnoActual : EstadoJuegoEnum by juegoViewModel.turnoActual().observeAsState(initial = EstadoJuegoEnum.TURNO_JUGADOR1)
@@ -78,7 +99,96 @@ fun TicTacToeScreen(juegoViewModel: JuegoViewModel) {
                 )
             }
 
-            Pie(modifier = Modifier.weight(1f).fillMaxWidth())
+            Pie(modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth())
+        }
+    }
+}
+
+
+@Composable
+fun TictactoeLandscape(juegoViewModel: JuegoViewModel) {
+    val estadoActualTablero : MutableList<DetalleCasillaTriqui> by juegoViewModel.estadoActualTablero().observeAsState(initial = emptyList<DetalleCasillaTriqui>().toMutableList())
+    val ganadorJuego: JugadorCasillaEnum by juegoViewModel.ganadorDelJuego().observeAsState(initial = JugadorCasillaEnum.NINGUNO)
+    val turnoActual : EstadoJuegoEnum by juegoViewModel.turnoActual().observeAsState(initial = EstadoJuegoEnum.TURNO_JUGADOR1)
+
+    ConstraintLayout(modifier = Modifier
+        .fillMaxSize()
+        .background(Purple80)) {
+        val (panelIzquierdo, panelDerecho) = createRefs()
+        ConstraintLayout(modifier = Modifier
+            .constrainAs(panelIzquierdo) {
+            bottom.linkTo(parent.bottom)
+            start.linkTo(parent.start)
+            top.linkTo(parent.top)
+            end.linkTo(panelDerecho.start)
+            width = Dimension.fillToConstraints
+            height = Dimension.fillToConstraints
+        }) {
+            val (encabezadoId, pieId) = createRefs()
+            val guidelineBottom = createGuidelineFromBottom(0.3f)
+            val guidelineTop = createGuidelineFromTop(0.2f)
+
+            Encabezado(modifier = Modifier
+                .constrainAs(encabezadoId){
+                bottom.linkTo(guidelineBottom)
+                end.linkTo(parent.end)
+                start.linkTo(parent.start)
+                top.linkTo(guidelineTop)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            }, turnoActual = turnoActual)
+
+            Pie(modifier = Modifier
+                .constrainAs(pieId){
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+                top.linkTo(guidelineBottom)
+                start.linkTo(parent.start)
+                width = Dimension.fillToConstraints
+                height = Dimension.fillToConstraints
+            })
+        }
+
+        ConstraintLayout(modifier = Modifier
+            .constrainAs(panelDerecho) {
+            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end)
+            start.linkTo(panelIzquierdo.end)
+            top.linkTo(parent.top)
+            width = Dimension.fillToConstraints
+            height = Dimension.fillToConstraints
+        }) {
+            val (tableroId, ganadorId) = createRefs()
+            if (ganadorJuego == JugadorCasillaEnum.NINGUNO) {
+                Tablero(
+                    modifier = Modifier.constrainAs(tableroId) {
+                        bottom.linkTo(parent.bottom)
+                        end.linkTo(parent.end)
+                        start.linkTo(parent.start)
+                        top.linkTo(parent.top)
+                        width = Dimension.fillToConstraints
+                        height = Dimension.fillToConstraints
+                    },
+                    estadoTablero = estadoActualTablero,
+                    clicable = {
+                        detalleCasillaTriqui ->
+                        juegoViewModel.turno(estadoJuegoEnum = turnoActual, detalleCasillaTriqui = detalleCasillaTriqui)
+                        juegoViewModel.notificarGanador()
+                    }
+                )
+            } else {
+                Ganador(modifier = Modifier.constrainAs(ganadorId){
+                    bottom.linkTo(parent.bottom)
+                    end.linkTo(parent.end)
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                },
+                    jugadorCasillaEnum = ganadorJuego,
+                    clicable = { juegoViewModel.reiniciarJuego() }
+                )
+            }
         }
     }
 }
